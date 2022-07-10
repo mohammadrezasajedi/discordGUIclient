4ab0cf85f016a8c68d0e4988f37566cffdd69e19
mohammadrezasajedi <mohammadsajedi927@gmail.com>
2022-07-10 16:13:19 +0430

    7th Commit

--- src/main/java/com/example/discordgui/controller/DinamicMenuController.java
+++ src/main/java/com/example/discordgui/controller/DinamicMenuController.java
@@ -5,7 +5,9 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import org.w3c.dom.events.Event;
 
 import java.io.IOException;
@@ -23,10 +25,27 @@ public class DinamicMenuController {
 
 
     public void setItems(ArrayList<String> items) {
+        if (items.size() > 10) {
+            menu.setPrefHeight(items.size() * 80);
+        }
         this.items = items;
         for (int i = 0; i < items.size(); i++) {
             Button button=new Button();
-            button.setText(items.get(i).split("\\.")[1]);
+            String str=items.get(i).split("\\.")[1];
+            if (str.contains(":")){
+                String[] strings = str.split(":");
+                str=strings[0];
+
+                StackPane stackPane=new StackPane();
+                stackPane.getStyleClass().add("notifArea");
+
+                Text text=new Text(strings[1]);
+                text.getStyleClass().add("notif");
+
+                stackPane.getChildren().add(text);
+                button.setGraphic(stackPane);
+            }
+            button.setText(str);
             button.setId(String.valueOf(i+1));
             button.setOnAction(actionEvent -> {
                 Button button1=(Button) actionEvent.getSource();
